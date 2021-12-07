d4d3ac8eadb8bb91e5faf1ab3f83fe584a5c174d
LoneHippie <lonehippie.music@gmail.com>
2021-12-07 16:49:59 +0200

    updated Gatsby version, added analytics and Netlify form

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
 module.exports = {
     plugins: [
         'gatsby-plugin-sass',
-        'gatsby-plugin-react-helmet'
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                trackingIds: [
+                process.env.GA_TRACKING_ID, // Google Analytics
+                ],
+                gtagConfig: {
+                optimize_id: "OPT_CONTAINER_ID",
+                anonymize_ip: true,
+                cookie_expires: 0,
+                },
+                pluginConfig: {
+                head: true,
+                respectDNT: true,
+                },
+            },
+        }
     ]
 }
--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -4,48 +4,122 @@ import './../styles/contactForm.scss';
 
 const ContactForm = () => {
 
-    const [ formSubject, setFormSubject ] = useState('');
-    const [ formBody, setFormBody ] = useState('');
+    const [ name, setName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ subject, setSubject ] = useState('');
+    const [ message, setMessage ] = useState('');
+
+    const successMessageToUser = `Thank you ${name}, your message has been sent. Expect to hear from us soon!`;
 
     const formHandlers = {
+        changeName: (e) => {
+            setName(e.target.value);
+        },
+        changeEmail: (e) => {
+            setEmail(e.target.value);
+        },
         changeSubject: (e) => {
-            setFormSubject(e.target.value)
+            setSubject(e.target.value);
         },
-        changeBody: (e) => {
-            setFormBody(e.target.value);
-        },
-        handleSubmit: (e) => {
-            e.preventDefault();
-            window.open(`mailto:lonehippie.dev@gmail.com?subject=${formSubject}&body=${formBody}`);
+        changeMessage: (e) => {
+            setMessage(e.target.value);
         }
+    };
+
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const formBody = {
+            name: name,
+            email: email,
+            subject: subject,
+            message: message
+        };
+
+        fetch('/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...formBody })
+        })
+        .then(() => {
+            alert(successMessageToUser);
+        })
+        .catch((err) => {
+            alert(err);
+        })
     }
 
     return (
         <div className="contact" id="contact">
             <div className="contact__form">
-                <form className="form" id="contact-form" onSubmit={(e) => formHandlers.handleSubmit(e)}>
+                <form 
+                    className="form" 
+                    name="contact"
+                    id="contact-form"
+                    method="POST"
+                    onSubmit={(e) => handleSubmit(e)}
+                    data-netlify={true}
+                    data-netlify-honeypot="bot-field"
+                >
+
+                    {/* <input type="hidden" name="bot-field" /> */}
+                    <input type="hidden" name="form-name" value="contact" />
 
                     <h2>Get in touch</h2>
 
                     <div className="form__group">
                         <input 
                             type="text" 
+                            name="name"
+                            className="form__input"
+                            placeholder="Name"
+                            value={name}
+                            onChange={(e) => formHandlers.changeName(e)}
+                            required={true}
+                        ></input>
+                        <label htmlFor="name" className="form__label">Name</label>
+                    </div>
+
+                    <div className="form__group">
+                        <input 
+                            type="text" 
+                            name="email"
+                            className="form__input"
+                            placeholder="Your Email"
+                            value={email}
+                            onChange={(e) => formHandlers.changeEmail(e)}
+                            required={true}
+                        ></input>
+                        <label htmlFor="email" className="form__label">Email</label>
+                    </div>
+
+                    <div className="form__group">
+                        <input 
+                            type="text" 
+                            name="subject"
                             className="form__input"
                             placeholder="Subject"
-                            value={formSubject}
+                            value={subject}
                             onChange={(e) => formHandlers.changeSubject(e)}
-                            required
+                            required={true}
                         ></input>
-                        <label htmlFor="name" className="form__label">Subject</label>
+                        <label htmlFor="subject" className="form__label">Subject</label>
                     </div>
 
                     <div className="form__group">
                         <textarea 
+                            name="message"
                             className="form__input form__textarea"
                             placeholder="Your Message" 
-                            value={formBody}
-                            onChange={(e) => formHandlers.changeBody(e)}
-                            required
+                            value={message}
+                            onChange={(e) => formHandlers.changeMessage(e)}
+                            required={true}
                         ></textarea>
                         <label htmlFor="message" className="form__label">Your Message</label>
                     </div>
