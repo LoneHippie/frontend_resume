f2224941a1545dadc24ae1658e047682130c4173
LoneHippie <lonehippie.music@gmail.com>
2024-06-04 20:40:56 +0300

    Fixed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,3 @@
 module.exports = {
-    plugins: [
-        'gatsby-plugin-sass',
-        'gatsby-plugin-react-helmet',
-        {
-            resolve: `gatsby-plugin-google-gtag`,
-            options: {
-                trackingIds: [
-                process.env.GA_TRACKING_ID, // Google Analytics
-                ],
-                gtagConfig: {
-                optimize_id: "OPT_CONTAINER_ID",
-                anonymize_ip: true,
-                cookie_expires: 0,
-                },
-                pluginConfig: {
-                head: true,
-                respectDNT: true,
-                },
-            },
-        }
-    ]
-}
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
+};
